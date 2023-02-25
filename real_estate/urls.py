ab1cc90c2996edac929c27b94aaf63069e8c6d49
squaredgithub <njugunafrancis077@gmail.com>
2023-02-25 08:57:55 +0300

    created user models

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('supersecret/', admin.site.urls),
+    path("supersecret/", admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to the Real Estate Portal"
