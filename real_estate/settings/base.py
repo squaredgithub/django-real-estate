1d86798fb611c4725b6a04d8e75b70a7860c4a30
squaredgithub <njugunafrancis077@gmail.com>
2023-03-06 21:11:01 +0300

    serrrrrver

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -25,9 +25,9 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 DJANGO_APPS = [
+    'django.contrib.contenttypes',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -43,6 +43,7 @@ THIRD_PARTY_APPS=[
     "phonenumber_field",
     "djoser",
     "rest_framework_simplejwt",
+    "djcelery_email",
 ]
 
 LOCAL_APPS = ["apps.common", "apps.users", "apps.profiles", "apps.ratings", "apps.properties", "apps.enquiries",]
